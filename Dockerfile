399acc010294dc61d668fc8ba3b442859a66254b
Patrick Wozniak <Pat@QueenAnne.local>
2018-03-13 21:10:45 -0400

    Updated config.  Added fswebcam to docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alexellis2/raspistill:latest
 ENTRYPOINT []
-RUN apt-get update -qy && apt-get install -qy python
+RUN apt-get update -qy && apt-get install -qy python && apt-get install -qy fswebcam
 COPY . .
 
 VOLUME /var/image/
--- config.py
+++ config.py
@@ -6,8 +6,8 @@ config["flip_horizontal"] = False
 config["flip_vertical"] = False
 config["metering_mode"] = "matrix"
 
-config["base_path"] = "/var/image"
-config["usb_path"] = "/var/image/usb"
+config["base_path"] = "/media/pi/UNTITLED/image"
+config["usb_path"] = "//media/pi/UNTITLED/usbimage"
 config["height"] = 1536
 config["width"] = 2048
 config["quality"] = 35
